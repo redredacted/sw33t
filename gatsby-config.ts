eaf6226e7c35df89c84f5213a99b09be3a234cdb
Kenny Sweet <redredacted@gmail.com>
2022-04-11 23:52:08 -0500

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `sw33t`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
